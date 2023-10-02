68926dbdbe66e8d2789f7d486f4910ed31d18bf8
MeidoNoHitsuji <popenkodaniil@mail.ru>
2023-10-03 01:54:07 +0700

    feat: Добавлена логика сервера обработки метрик

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import "net/http"
+
+var Store MemStorage
+
+func main() {
+	Store = MemStorage{
+		mGauge:   make(map[string]float64),
+		mFloat64: make(map[string]float64),
+		mCounter: make(map[string]int64),
+		mInt64:   make(map[string]int64),
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", AddMetric)
+
+	err := http.ListenAndServe(":8080", OnlyPOTS(mux))
+	if err != nil {
+		return
+	}
+}
