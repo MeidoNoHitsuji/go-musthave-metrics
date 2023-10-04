129c3d4a561eb17cd4d695bc5583cf88fdbe8469
MeidoNoHitsuji <popenkodaniil@mail.ru>
2023-10-05 01:13:08 +0700

    feat: Начальная логика агента

---
+++ cmd/agent/handlers/handlers.go
@@ -0,0 +1,91 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/MeidoNoHitsuji/go-musthave-metrics/cmd/agent/storage"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+type MetricType string
+
+var RStats runtime.MemStats
+
+const (
+	GAUGE   = MetricType("gauge")
+	FLOAT   = MetricType("float64")
+	COUNTER = MetricType("counter")
+	INT     = MetricType("int64")
+)
+
+func SendMetric(m MetricType, name string, value string) (interface{}, error) {
+	resp, err := http.Post(fmt.Sprintf("http://localhost:8080/update/%s/%s/%s", m, name, value), "text/plain", strings.NewReader(""))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return nil, nil
+	//var res map[string]interface{}
+	//err = json.NewDecoder(resp.Body).Decode(&res)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//form := res["form"]
+	//return form, nil
+}
+
+func LoadMetric() {
+	runtime.ReadMemStats(&RStats)
+	storage.Store.AddGauge("Alloc", RStats.Alloc)
+	storage.Store.AddGauge("BuckHashSys", RStats.BuckHashSys)
+	storage.Store.AddGauge("Frees", RStats.Frees)
+	storage.Store.AddGauge("GCCPUFraction", RStats.GCCPUFraction)
+	storage.Store.AddGauge("GCSys", RStats.GCSys)
+	storage.Store.AddGauge("HeapAlloc", RStats.HeapAlloc)
+	storage.Store.AddGauge("HeapIdle", RStats.HeapIdle)
+	storage.Store.AddGauge("HeapInuse", RStats.HeapInuse)
+	storage.Store.AddGauge("HeapObjects", RStats.HeapObjects)
+	storage.Store.AddGauge("HeapReleased", RStats.HeapReleased)
+	storage.Store.AddGauge("HeapSys", RStats.HeapSys)
+	storage.Store.AddGauge("LastGC", RStats.LastGC)
+	storage.Store.AddGauge("Lookups", RStats.Lookups)
+	storage.Store.AddGauge("MCacheInuse", RStats.MCacheInuse)
+	storage.Store.AddGauge("MCacheSys", RStats.MCacheSys)
+	storage.Store.AddGauge("MSpanInuse", RStats.MSpanInuse)
+	storage.Store.AddGauge("MSpanSys", RStats.MSpanSys)
+	storage.Store.AddGauge("Mallocs", RStats.Mallocs)
+	storage.Store.AddGauge("NextGC", RStats.NextGC)
+	storage.Store.AddGauge("NumForcedGC", RStats.NumForcedGC)
+	storage.Store.AddGauge("NumGC", RStats.NumGC)
+	storage.Store.AddGauge("OtherSys", RStats.OtherSys)
+	storage.Store.AddGauge("PauseTotalNs", RStats.PauseTotalNs)
+	storage.Store.AddGauge("StackInuse", RStats.StackInuse)
+	storage.Store.AddGauge("StackSys", RStats.StackSys)
+	storage.Store.AddGauge("Sys", RStats.Sys)
+	storage.Store.AddGauge("TotalAlloc", RStats.TotalAlloc)
+	storage.Store.AddGauge("RandomValue", rand.Float64())
+	storage.Store.AddCounter("PollCount", 1)
+}
+
+func SendMetrics() {
+	for k, v := range storage.Store.MGauge {
+		_, err := SendMetric(GAUGE, k, v)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	storage.Store.MGauge = make(map[string]string)
+
+	for k, v := range storage.Store.MCounter {
+		_, err := SendMetric(COUNTER, k, strconv.FormatInt(v, 10))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	storage.Store.MCounter = make(map[string]int64)
+}
--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"github.com/MeidoNoHitsuji/go-musthave-metrics/cmd/agent/handlers"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			handlers.LoadMetric()
+			log.Printf("Метрики собраны")
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(10 * time.Second)
+			handlers.SendMetrics()
+			log.Printf("Метрики отправлены")
+		}
+	}()
+
+	log.Printf("Сбор метрик запущен")
+	Hold()
+	log.Printf("Сбор метрик остановлен")
+}
+
+func Hold() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+		syscall.SIGUSR1)
+	<-sigChan
+}
