27fd7abad8e933d8bafca6ca446b1307b0ca6347
MeidoNoHitsuji <popenkodaniil@mail.ru>
2023-10-06 21:41:23 +0700

    feat: Сервер переписан с использованием библиотеки chi

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"github.com/MeidoNoHitsuji/go-musthave-metrics/cmd/server/handlers"
-	"github.com/MeidoNoHitsuji/go-musthave-metrics/cmd/server/middlewares"
+	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", handlers.AddMetric)
+	r := chi.NewRouter()
+
+	r.Get("/", handlers.GetMetrics)
+
+	r.Route("/update", func(r chi.Router) {
+		r.Post("/gauge/{key}/{value}", handlers.AddGauge)
+		r.Post("/float64/{key}/{value}", handlers.AddFloat)
+		r.Post("/counter/{key}/{value}", handlers.AddCounter)
+		r.Post("/int64/{key}/{value}", handlers.AddInt)
+	})
+
+	r.Route("/value", func(r chi.Router) {
+		r.Get("/gauge/{key}", handlers.GetGauge)
+		r.Get("/float64/{key}", handlers.GetFloat)
+		r.Get("/counter/{key}", handlers.GetCounter)
+		r.Get("/int64/{key}", handlers.GetInt)
+	})
 
 	log.Printf("Сервер запущен")
-	err := http.ListenAndServe(":8080", middlewares.OnlyPOTS(mux))
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
