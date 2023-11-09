865d9f1885dbd8795a043af54ac119c2cd4c508a
MeidoNoHitsuji <popenkodaniil@mail.ru>
2023-11-09 22:46:16 +0700

    feat: Мидлвар логов подключён к запросам

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,15 @@ package server
 
 import (
 	"github.com/MeidoNoHitsuji/go-musthave-metrics/internal/handlers"
+	"github.com/MeidoNoHitsuji/go-musthave-metrics/internal/middlewares"
 	"github.com/go-chi/chi/v5"
 )
 
 func Router() chi.Router {
 	r := chi.NewRouter()
 	handler := handlers.New()
+
+	r.Use(middlewares.WithLogging)
 
 	r.Get("/", handler.GetMetrics)
 	r.Post("/update/{type}/{key}/{value}", handler.AddMetric)
